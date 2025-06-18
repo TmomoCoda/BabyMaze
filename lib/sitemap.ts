6e38008d8c9e6d1b7a83085a02e37023499a0637
TmomoCoda <Kotapeya@gmail.com>
2025-06-18 18:53:06 +0200

    Remove Next.js setup and configs

--- lib/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from 'next';
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: 'https://freegames4eva.com/', lastModified: new Date() },
-    // TODO: fetch slugs
-  ];
-}
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: true },
-  images: {
-    domains: ['example.com'] // TODO: add CDN domain
-  },
-};
-export default nextConfig;
